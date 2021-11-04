91fb25e54571118d6cfb2ff18363b9c9a7e3fdc8
suyeon <rlatndus9911@naver.com>
2021-11-04 12:00:52 +0900

    ✨[FEAT] Tabbar 연결 (#1)

--- Carrot_Market_iOS/Carrot_Market_iOS/Sources/CarrotMarket/CustomTabBarController.swift
+++ Carrot_Market_iOS/Carrot_Market_iOS/Sources/CarrotMarket/CustomTabBarController.swift
@@ -16,12 +16,21 @@ class CustomTabBarController: UITabBarController {
     }
     
     func setTabBar() {
-        guard let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeVC")
+        guard let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeVC"),
+              let townVC = self.storyboard?.instantiateViewController(withIdentifier: "TownVC"),
+              let myAroundVC = self.storyboard?.instantiateViewController(withIdentifier: "MyAroundVC"),
+              let chatVC = self.storyboard?.instantiateViewController(withIdentifier: "ChatVC"),
+              let myCarrotVC = self.storyboard?.instantiateViewController(withIdentifier: "MyCarrotVC")
         else { return }
         
-        homeVC.tabBarItem = UITabBarItem(title: "홈", image: UIImage(named: "homeIcon"), selectedImage: UIImage(named: "homeIconFill"))
         
-        setViewControllers([homeVC], animated: true)
+        homeVC.tabBarItem = UITabBarItem(title: "홈", image: UIImage(named: "homeIcon"), selectedImage: UIImage(named: "homeIconFill"))
+        townVC.tabBarItem = UITabBarItem(title: "동네생활", image: UIImage(named: "townIcon"), selectedImage: UIImage(named: "townIconFill"))
+        myAroundVC.tabBarItem = UITabBarItem(title: "내 근처", image: UIImage(named: "myAroundIcon"), selectedImage: UIImage(named: "myAroundIconFill"))
+        chatVC.tabBarItem = UITabBarItem(title: "채팅", image: UIImage(named: "chatIcon"), selectedImage: UIImage(named: "chatIconFill"))
+        myCarrotVC.tabBarItem = UITabBarItem(title: "나의 당근 ", image: UIImage(named: "myCarrotIcon"), selectedImage: UIImage(named: "myCarrotIconFill"))
+        
+        setViewControllers([homeVC,townVC,myAroundVC,chatVC,myCarrotVC], animated: true)
     }
     
     func setTabBarStyle() {
